4a12d3d5739dc544c3479e46aecf4a4b8187d496
AzizBelHadjHmida <azizb2h@gmail.com>
2025-05-04 19:58:12 +0100

    user fixture

---
+++ src/DataFixtures/UtilisateurFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class UtilisateurFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for($i=0; $i<10;$i++){
+            $utilisateur = new Utilisateur();
+            $utilisateur->setEmail($faker->email());
+            $utilisateur->setPassword($faker->password());
+            $utilisateur->setName($faker->name());
+            $utilisateur->setAdress($faker->address());
+//            $role = $faker->randomElement(['ROLE_ADMIN', 'ROLE_USER']);
+//            $utilisateur->setRoles($role);
+
+            $manager->persist($utilisateur);
+
+        }
+
+        $manager->flush();
+    }
+}
