6d960da842c127d425cf8c7fc226ad1c070700aa
AzizBelHadjHmida <azizb2h@gmail.com>
2025-05-06 20:10:23 +0100

    added service layer for command

--- src/Controller/CommandController.php
+++ src/Controller/CommandController.php
@@ -6,15 +6,21 @@ use App\Entity\Cart;
 use App\Entity\Commande;
 use App\Form\CheckoutForm;
 use App\Repository\CartRepository;
+use App\Service\CommandService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use function Webmozart\Assert\Tests\StaticAnalysis\float;
 
-final class CheckoutController extends AbstractController
+
+final class CommandController extends AbstractController
 {
+    private commandService $commandService;
+    public function __construct(commandService $commandService)
+    {
+        $this->commandService = $commandService;
+    }
     #[Route('/checkout/{id}', name: 'app_checkout')]
 
     public function index(Cart $cart): Response
@@ -23,16 +29,10 @@ final class CheckoutController extends AbstractController
             $this->addFlash('error', 'No cart found for the current user.');
             return $this->redirectToRoute('app_index');
         }
-            $cartItems = $cart->getCartItems();
-             $total = 0;
-            foreach ($cartItems as $cartItem) {
-
-                $total += $cartItem->getProduit()->getPrice() * $cartItem->getQuantité();
-            }
-
+        $total=$this->commandService->calculateTotal($cart);
 
         return $this->render('checkout/index.html.twig', [
-            'cartItems' => $cartItems,
+            'cartItems' => $cart->getCartItems(),
             'cart'=>$cart,
             'total' => $total,
 
@@ -40,26 +40,15 @@ final class CheckoutController extends AbstractController
     }
     #[Route('/checkout/order/{id}', name: 'app_place_order')]
 
-    public function placeOrder(Cart $cart,EntityManagerInterface $em): Response
+    public function placeOrder(Cart $cart): Response
     {
         if (!$cart) {
             $this->addFlash('error', 'No cart found for the current user.');
             return $this->redirectToRoute('app_index');
         }
-
-            $cartItems = $cart->getCartItems();
-            $commande = new Commande();
-            $commande->setUtilisateur($cart->getUtilisateur()->getUtilisateur());
-            foreach ($cartItems as $cartItem) {
-                $commande->addCartItem($cartItem);
-                $cartItem->setCommande($commande);
-                $cartItem->setCart(null); // Remove the cart association
-
-            }
+        $this->commandService->manageCommand($cart);
 
 
-            $em->persist($commande);
-            $em->flush();
         $this->addFlash('success', 'Your order has been placed successfully!');
         return $this->redirectToRoute('app_cart');
     }
