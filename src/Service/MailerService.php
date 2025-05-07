59c81d1b805c512d240588021d8483fa6d502244
AzizBelHadjHmida <azizb2h@gmail.com>
2025-05-07 19:12:49 +0100

    added mailer and login services

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
-class MailerController extends AbstractController
+class MailerService extends AbstractController
 {
     #[Route('/verify/email', name: 'verify_email')]
     public function verifyUserEmail(Request $request, EmailVerifier $emailVerifier, EntityManagerInterface $entityManager): Response
